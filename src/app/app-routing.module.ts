fcff44fd0109ca907ecc5b0e8343e24613e11851
Aouadhi <aymen.aouadhi@live.fr>
2020-09-16 13:56:03 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/public/login/login.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { ProfileComponent } from './components/private/profile/profile.component';
+import { DashboardComponent } from './components/private/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+      component: LoginComponent
+  },
+  {
+    path: '',
+      component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { RegisterComponent } from './components/public/register/register.component';
+import { ProfileComponent } from './components/private/profile/profile.component';
+import { DashboardComponent } from './components/private/dashboard/dashboard.component';
+import { NavbarComponent } from './components/public/navbar/navbar.component';
+import { FooterComponent } from './components/public/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    ProfileComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
